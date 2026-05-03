10ef3c71dd1659e8a911c2c0a605b09c379369c5
DawrAli33285 <lemightyeagle@gmail.com>
2026-05-03 14:59:44 +0500

    new

--- src/components/baseurl.js
+++ src/components/baseurl.js
@@ -1,3 +1,3 @@
-export const BASE_URL="https://www.patto.pro/api"
-// export const BASE_URL="http://localhost:5000/api"
+// export const BASE_URL="https://www.patto.pro/api"
+export const BASE_URL="http://localhost:5000/api"
 // export const BASE_URL="https://datingbackend-seven.vercel.app"
